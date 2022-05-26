cd459d9b9ba6bd385abd5bbf2a8bd5ab700aba56
pierreneillo <75447740+pierreneillo@users.noreply.github.com>
2022-05-26 22:34:15 +0200

    Creation of Client class + a few basic methods

--- client.py
+++ client.py
@@ -1,7 +1,37 @@
 import socket
-connexion_serveur = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print("Attempting connexion on port 10005")
-connexion_serveur.connect(('127.0.0.1',10005))
-print("Sucessful")
-connexion_serveur.send(b"Hello")
-print(connexion_serveur.recv(1024))
+
+class Client:
+    def __init__(self,pseudo,host="127.0.0.1",port=10005):
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.host = host
+        self.port = port
+        self.pseudo = pseudo
+        print(f"Attempting connexion on port {self.port}...")
+        self.socket.connect((self.host,self.port))
+        print("Connexion sucessful")
+    def log(self):
+        print("Logging...")
+        self.socket.send(f"Log: {self.pseudo}".encode("utf-8"))
+        status = self.socket.recv(1024)
+        status = int(status.decode())
+        if status==200:
+            print("Login sucessful")
+        elif status==403:
+            print("Access forbidden")
+        elif status==404:
+            print(f"No such existing account '{self.pseudo}'")
+        else:
+            print(f"Error {status}")
+    def signup(self):
+        print("Crating your account...")
+        self.socket.send(f"Create: {self.pseudo}".encode("utf-8"))
+        status = self.socket.recv(1024)
+        status = int(status.decode())
+        if status==200:
+            print("Sign up sucessful")
+        elif status==403:
+            print("Forbidden pseudo")
+        elif status==404:
+            print("An account already exists with this pseudo")
+        else:
+            print(f"Error {status}")
