8bc20c57b9d98f86ff4f9315305ab5e507726376
pierreneillo <75447740+pierreneillo@users.noreply.github.com>
2022-05-26 23:21:36 +0200

    Implemented secure communication on server-side

--- serveur.py
+++ serveur.py
@@ -1,11 +1,14 @@
 import socket
 import select
+import RSA
 
 class Server:
     known_logs = ["root_user"]
     def __init__(self, host = 'localhost', port = 10005, max_listen = 5):
         self.host = host
         self.port = port
+        self.clePub,self.clePriv = RSA.generation_cle_RSA()
+        print(self.clePub,self.clePriv)
         self.max_listen = max_listen
         self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#Création du socket
         self.socket.setblocking(0)
@@ -13,7 +16,8 @@ class Server:
         self.socket.listen(5)
         self.clients=[]
         self.clients_pseudos=[]
-        self.messages=[]
+        self.keys = []
+        self.messages = []
     def run(self):
         print(f"server running on port {self.port}")
         while True:
@@ -24,6 +28,7 @@ class Server:
                 connexion.setblocking(0)
                 self.clients.append(client_connexion)
                 self.clients_pseudos.append(None)
+                self.keys.append(None)
             try:
                 client_requests, wlist, xlist = select.select(self.clients,[],[],0.05)
             except select.error:
@@ -60,6 +65,17 @@ class Server:
                             Server.known_logs.append(log)
                         print(f"***SIGNUP REQUEST***\nFrom client n°{id_sender}\nRequested log: {log}\nStatus: {status}\n")
                         client.send(str(status).encode("utf-8"))
+                    #SECURE REQUESTS TREATMENT
+                    elif received_message.startswith("Secure: "):
+                        secure_method = received_message[8:]
+                        if secure_method=="RSA + ext_vig_256":
+                            client.send(f"{self.clePub[0]},{self.clePub[1]}".encode("utf-8"))
+                        else:
+                            client.send(b"Unknown securisation method")
+                    elif received_message.startswith("SymKey: "):
+                        encoded_key = received_message[8:]
+                        self.keys[id_sender] = RSA.dechiffrement_RSA(encoded_key,self.clePriv)
+                        print(self.keys[id_sender])
 
 
 server1 = Server()
